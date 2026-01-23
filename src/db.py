b46eac41c5f1057da1f3ea3843c5b1c907e54c69
ifluency <152925865+ifluency@users.noreply.github.com>
2026-01-23 09:34:00 -0300

    Update db.py

--- src/db.py
+++ src/db.py
@@ -2,10 +2,7 @@ import os
 import psycopg2
 
 def get_conn():
-    db_url = os.environ["DATABASE_URL"].strip()  # <- remove \n e espaços
-
-    # garante sslmode=require
+    db_url = os.environ["DATABASE_URL"].strip()  # remove \n e espaços
     if "sslmode=" not in db_url:
         db_url += "&sslmode=require" if "?" in db_url else "?sslmode=require"
-
     return psycopg2.connect(db_url)
