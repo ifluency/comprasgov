252b13d29fe9bf4cc2b738ab6247371792ab2ab1
ifluency <152925865+ifluency@users.noreply.github.com>
2026-01-23 16:23:23 -0300

    Update 002_indexes.sql

--- migrations/002_indexes.sql
+++ migrations/002_indexes.sql
@@ -1,14 +1,11 @@
-create index if not exists idx_licitacao_uasg_data_publicacao
-  on licitacao (uasg, data_publicacao);
+create index if not exists idx_contratacao_objeto_gin
+  on contratacao_pncp_14133 using gin (to_tsvector('portuguese', coalesce(objeto,'')));
 
-create index if not exists idx_licitacao_dt_alteracao
-  on licitacao (dt_alteracao);
+create index if not exists idx_contratacao_codigo_orgao
+  on contratacao_pncp_14133 (codigo_orgao);
 
-create index if not exists idx_licitacao_numero_aviso
-  on licitacao (numero_aviso);
+create index if not exists idx_contratacao_orgao_cnpj
+  on contratacao_pncp_14133 (orgao_cnpj);
 
-create index if not exists idx_licitacao_modalidade
-  on licitacao (modalidade);
-
-create index if not exists idx_licitacao_objeto_gin
-  on licitacao using gin (to_tsvector('portuguese', coalesce(objeto,'')));
+create index if not exists idx_contratacao_excluida
+  on contratacao_pncp_14133 (contratacao_excluida);
