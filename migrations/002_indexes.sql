ec3a911a3b81f1861f019aebfa327f26d484934f
ifluency <152925865+ifluency@users.noreply.github.com>
2026-01-24 19:20:58 -0300

    Update 002_indexes.sql

--- migrations/002_indexes.sql
+++ migrations/002_indexes.sql
@@ -1,11 +1,17 @@
+create index if not exists idx_contratacao_unidade_modalidade_pub
+  on contratacao_pncp_14133 (unidade_orgao_codigo_unidade, codigo_modalidade, data_publicacao_pncp);
+
+create index if not exists idx_contratacao_data_atualizacao
+  on contratacao_pncp_14133 (data_atualizacao_pncp);
+
+create index if not exists idx_contratacao_id_compra
+  on contratacao_pncp_14133 (id_compra);
+
+create index if not exists idx_contratacao_numero_compra
+  on contratacao_pncp_14133 (numero_compra);
+
+create index if not exists idx_contratacao_processo
+  on contratacao_pncp_14133 (processo);
+
 create index if not exists idx_contratacao_objeto_gin
-  on contratacao_pncp_14133 using gin (to_tsvector('portuguese', coalesce(objeto,'')));
-
-create index if not exists idx_contratacao_codigo_orgao
-  on contratacao_pncp_14133 (codigo_orgao);
-
-create index if not exists idx_contratacao_orgao_cnpj
-  on contratacao_pncp_14133 (orgao_cnpj);
-
-create index if not exists idx_contratacao_excluida
-  on contratacao_pncp_14133 (contratacao_excluida);
+  on contratacao_pncp_14133 using gin (to_tsvector('portuguese', coalesce(objeto_compra,'')));
